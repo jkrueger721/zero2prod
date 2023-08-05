d73080c12ce2d23aeb268c1af4862be3bdf0b604
jkrueger721 <johnathankrueger@me.com>
2023-08-05 13:25:36 -0400

    remove async func

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
@@ -13,6 +13,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length())
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    todo!()
+fn spawn_app() {
+    let server = zero2prod::run().expect("failed to bind address");
+
+    let _ = tokio::spawn(server);
 }
