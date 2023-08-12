4527548df317b563460a4c6917f21654200da784
jkrueger721 <johnathankrueger@me.com>
2023-08-11 23:09:24 -0400

    added config and db connection

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::Connection;
+use zero2prod::configuration::get_configuration;
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -29,6 +32,12 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = sqlx::PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_quin%40gmail.com";
@@ -41,6 +50,14 @@ async fn subribe_returns_a_200_for_valid_form_data() {
         .expect("failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    
+    assert_eq!(saved.email, "ursula_le_quin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 #[tokio::test]
 async fn subcribe_returns_a_400_when_data_is_missing() {
