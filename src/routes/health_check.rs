e0fb1fe0fd4f81364379ec5086cde29476426c92
John Krueger <johnathankrueger@me.com>
2024-03-18 09:06:57 -0400

    name validation

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,6 @@
 use actix_web::HttpResponse;
+use serde_json::json;
 
 pub async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().json(json!({"message": "we are good to go"}))
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use crate::domain::{NewSubscriber,SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -16,9 +17,17 @@ pub struct FormData {
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
-)]
+)] 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: name
+    };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
@@ -29,18 +38,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
             "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
